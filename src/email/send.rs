a5b1cffd630a4637d7809857959ce956b6443032
Johan Montorfano <me@johanmontorfano.com>
2023-06-04 22:06:34 +0200

    solved an issue that leads to crashes when having a malformed identity

--- src/email/send.rs
+++ src/email/send.rs
@@ -1,7 +1,7 @@
 use crate::config::generic::GenConfEmail;
 use lettre::{
     message::Mailbox,
-    transport::smtp::{authentication::Credentials, response::Response, Error},
+    transport::smtp::{authentication::Credentials, response::Response},
     Message, SmtpTransport, Transport,
 };
 
@@ -40,36 +40,38 @@ impl Turkey {
     }
 
     // Send an email.
-    pub fn send_email(&self, email: Email) -> Result<Response, Error> {
+    pub fn send_email(&self, email: Email) -> Result<Response, String> {
         // Build mailboxes now to prevent any `MissingParts` server crash.
         let source_mailbox =
             format!("{} <{}>", email.from_name, self.email_config_copy.address).parse::<Mailbox>();
+        let recipient_mailbox =
+            format!("{} <{}>", email.to_name, email.to_email).parse::<Mailbox>();
+        let cc_mailbox =
+            format!("{} <{}>", email.cc_name.unwrap_or("".into()), email.cc_email.unwrap_or("".into())).parse::<Mailbox>();
 
-        // Creates the email.
-        let mut email_builder = Message::builder()
-            .from(
-                format!("{} <{}>", email.from_name, self.email_config_copy.address)
-                    .parse()
-                    .unwrap(),
-            )
-            .to(format!("{} <{}>", email.to_name, email.to_email)
-                .parse()
-                .unwrap())
-            .subject(email.subject);
+        if source_mailbox.is_err() || recipient_mailbox.is_err() {
+            return Err("Malformed source/recipient identity: format it as 'John Doe <johndoe@glouglou.johanmontorfano.com>'".into());
+        } else {
+            let source_mailbox = source_mailbox.unwrap();
+            let recipient_mailbox = recipient_mailbox.unwrap();
 
-        // Determines if there is a CC, otherwise it does nothing.
-        if email.cc_email.is_some() && email.cc_name.is_some() {
-            email_builder = email_builder.cc(format!(
-                "{} <{}>",
-                email.cc_name.unwrap(),
-                email.cc_email.unwrap()
-            )
-            .parse()
-            .unwrap());
+            // Creates the email.
+            let mut email_builder = Message::builder()
+                .from(source_mailbox)
+                .to(recipient_mailbox)
+                .subject(email.subject);
+
+            // If parsing has been done for CC, it means that a CC has been used and will be added to the email.
+            if cc_mailbox.is_ok() {
+                email_builder = email_builder.cc(cc_mailbox.unwrap());
+            }
+
+            let email = email_builder.body(email.body).unwrap();
+
+            return match self.connection.send(&email) {
+                Ok(response) => Ok(response),
+                Err(reason) => Err(reason.to_string()),
+            };
         }
-
-        let email = email_builder.body(email.body).unwrap();
-
-        return self.connection.send(&email);
     }
 }
